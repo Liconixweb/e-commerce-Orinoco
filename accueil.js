12c58991d7a2f5b5bf2b6b162b2e30082f485090
Delphine Marmaroli <petitefee.pro@gmail.com>
2021-02-02 16:35:48 +0100

    Mise en place de l'API dans la page d'accueil

--- accueil.js
+++ accueil.js
@@ -1,10 +1,27 @@
+//Récupération de l'API
+
 fetch("http://localhost:3000/api/teddies")
 .then(response => response.json())
-.then(data => {
-    
-    for(let i=0; i <data.length; i++){       
-        console.log('Ours ' + i + ' ' + data[i].name);
-    }
-    //instructions 
+.then(ours => {
+    for(let i=0; i <ours.length; i++){      
+        console.log('Ours ' + i + ' ' + ours[i].imageUrl + ' ' + ours[i].name + ' ' + ours[i].price);
+        main = document.getElementById('main');
+        newOurs = document.createElement('div');
+        main.append(newOurs);
+        newOurs.className = 'ours col-6';
+        newOursName = document.createElement("div");        
+        newOursName.className = 'oursName';
+        newOursName.textContent = ours[i].name;
+        newOurs.append(newOursName);
+        newOursImage = document.createElement("img");    
+        newOursImage.src = ours[i].imageUrl;    
+        newOursImage.className = 'oursImage';
+        newOursImage.textContent = ours[i].imageUrl;
+        newOurs.append(newOursImage);
+        newOursPrice = document.createElement("div");
+        newOursPrice.className = 'oursPrice';
+        newOursPrice.textContent = ours[i].price;
+        newOurs.append(newOursPrice);
+    } 
 })
 .catch(error => alert("Erreur : " + error));
