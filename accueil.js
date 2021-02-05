71c12d10c19fe8c1319291bcc32d5e88efe45953
Delphine Marmaroli <petitefee.pro@gmail.com>
2021-02-05 12:14:38 +0100

    Mise en place de SASS et Bootstrap

--- accueil.js
+++ accueil.js
@@ -1,4 +1,4 @@
-//Récupération de l'API
+//Récupération de l'API et création de chaque fiche ours
 
 fetch("http://localhost:3000/api/teddies")
 .then(response => response.json())
@@ -7,21 +7,25 @@ fetch("http://localhost:3000/api/teddies")
         console.log('Ours ' + i + ' ' + ours[i].imageUrl + ' ' + ours[i].name + ' ' + ours[i].price);
         main = document.getElementById('main');
         newOurs = document.createElement('div');
-        main.append(newOurs);
-        newOurs.className = 'ours col-6';
-        newOursName = document.createElement("div");        
-        newOursName.className = 'oursName';
-        newOursName.textContent = ours[i].name;
-        newOurs.append(newOursName);
+        newOurs.className = 'ours row mx-auto  mt-2';
+        main.append(newOurs);        
         newOursImage = document.createElement("img");    
         newOursImage.src = ours[i].imageUrl;    
-        newOursImage.className = 'oursImage';
+        newOursImage.className = 'oursImage img-thumbnail rounded max-auto d-block mb-2 roboto';
         newOursImage.textContent = ours[i].imageUrl;
         newOurs.append(newOursImage);
-        newOursPrice = document.createElement("div");
-        newOursPrice.className = 'oursPrice';
+        newOursName = document.createElement("h2");        
+        newOursName.className = 'oursName col-9';
+        newOursName.textContent = ours[i].name;
+        newOurs.append(newOursName);
+        newOursPrice = document.createElement("h3");
+        newOursPrice.className = 'oursPrice col-3';
         newOursPrice.textContent = ours[i].price;
         newOurs.append(newOursPrice);
+        newOursDescription = document.createElement("h4");
+        newOursDescription.className = 'oursDescription col text-center';
+        newOursDescription.textContent = "Plusieurs coloris";
+        newOurs.append(newOursDescription);
     } 
 })
 .catch(error => alert("Erreur : " + error));
