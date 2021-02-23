3c9e2841525b2bb94b5436547f82ca4070ce7871
Delphine Marmaroli <petitefee.pro@gmail.com>
2021-02-23 17:15:49 +0100

    Réglage de la mise en page de la page panier

--- js/panier.js
+++ js/panier.js
@@ -2,7 +2,7 @@
 
 main = document.getElementById('main-panier');
 newPanier = document.createElement('div');
-newPanier.className = 'newPanier row mx-auto mt-2 mb-2 col-md-6';
+newPanier.className = 'newPanier col m-2';
 main.prepend(newPanier);
 
 //Récupération des données du panier dans le localStorage
@@ -24,7 +24,7 @@ if (panierStocke != null) {
         newPanier.appendChild(newPanierTitle);
 
         oursCommandeTitle = document.createElement('p');
-        oursCommandeTitle.className = 'oursCommandeTitle row mx-auto mt-2 mb-3 col-md-6';
+        oursCommandeTitle.className = 'oursCommandeTitle row mt-2 mb-2';
         oursCommandeTitle.textContent = 'Les ours commandés : ';
         newPanier.appendChild(oursCommandeTitle);
 
@@ -35,26 +35,26 @@ if (panierStocke != null) {
         for(let i=0; i < panierStocke.length; i++){      
 
             oursPanier = document.createElement('div');
-            oursPanier.className = 'oursPanier mx-auto row mt-2 mb-3 col-md-6';
+            oursPanier.className = 'oursPanier row d-flex align-items-center mt-2 mb-3 m-md-0 h-md-15';
             newPanier.appendChild(oursPanier);
 
             oursPanierImage = document.createElement('img');    
             oursPanierImage.src = panierStocke[i].imageUrl;
-            oursPanierImage.className = 'oursPanierImage col max-auto text-center w-50 h-50 mt-4 mb-2 img-fluid';
+            oursPanierImage.className = 'oursPanierImage col align-self-center w-50 h-50 mt-2 mb-2 img-fluid';
             oursPanierImage.textContent = panierStocke[i].imageUrl;
             oursPanier.appendChild(oursPanierImage);
 
             oursPanierTitle = document.createElement('p');
-            oursPanierTitle.className = 'oursPanierTitle col ml-2 pl-4 align-self-center';
+            oursPanierTitle.className = 'oursPanierTitle col m-0 align-self-center justify-content-center text-center';
             oursPanierTitle.textContent = panierStocke[i].name;
             oursPanier.appendChild(oursPanierTitle);
 
         // Gestion du prix des articles commandés
 
             oursPanierPrice = document.createElement('p');
-            oursPanierPrice.className = 'oursPanierPrice col mt-4';
+            oursPanierPrice.className = 'oursPanierPrice col m-0 flex-nowrap align-self-center justify-content-center text-center';
             oursPanierPrice.setAttribute('id', 'prixArticle');
-            oursPanierPrice.textContent = 'Prix total : ' + panierStocke[i].price + " €";
+            oursPanierPrice.textContent = 'Prix total : ' + panierStocke[i].price + "€";
             oursPanier.appendChild(oursPanierPrice); 
             
             totalCommande = totalCommande + panierStocke[i].price;
@@ -62,13 +62,13 @@ if (panierStocke != null) {
         //Suppression de l'article après un clique
 
             buttonSupprime = document.createElement('button');
-            buttonSupprime.className = 'buttonSupprime fas fa-trash-alt  btn btn-outline-secondary text-center';
+            buttonSupprime.className = 'buttonSupprime fas fa-trash-alt col-md btn btn-outline-secondary text-center';
             oursPanier.appendChild(buttonSupprime);
             
             console.log(panierStocke);          
             
             buttonSupprime.addEventListener('click', function supprimeArticle(event){                  
-                let index = i.target;                
+                let index = event.target;                
                 panierStocke.splice(index,1);             
                 localStorage.setItem('panier',JSON.stringify(panierStocke));
                 window.location.href = "panier.html";
@@ -81,8 +81,8 @@ if (panierStocke != null) {
         //Calcul du total de la commande
 
     oursCommandeTotal = document.createElement('p');
-    oursCommandeTotal.className = 'oursCommandeTotal row mx-auto mt-2 mb-3 col-md-6';    
-    oursCommandeTotal.textContent = 'Total de la commande : ' + totalCommande + ' €';
+    oursCommandeTotal.className = 'oursCommandeTotal row mx-auto mt-2 mb-3';    
+    oursCommandeTotal.textContent = 'Total de la commande : ' + totalCommande + '€';
     newPanier.appendChild(oursCommandeTotal);
     }
 } else {
@@ -104,26 +104,25 @@ console.log(commande);
 
 valider = document.getElementById('valide');
 let firstName = document.getElementById('nom');
-let regexFirstName = /[a-zA-Z]\.\s/g;
+let regexFirstName = /([a-zA-Z\-])\s([a-zA-Z])/g;
 let lastName = document.getElementById('prenom');
-let regexLastName = /[a-zA-Z]\.\s/g;
+let regexLastName = /([a-zA-Z\-])\s([a-zA-Z])/g;
 let address = document.getElementById('adresse');
-let regexAdress = /\w\.\s\n/g;
+let regexAdress = /[a-zA-Z0-9\-_\.]\s[a-zA-Z0-9\-_\.]/g;
 let codeAddress = document.getElementById('code-adresse');
-let regexCodeAddress = /\d\s/g;
+let regexCodeAddress = /[0-9]/g;
 let city = document.getElementById('ville');
-let regexCity = /[a-zA-Z]\.\s/g;
+let regexCity = /([a-zA-Z\-])\s([a-zA-Z])/g;
 let email = document.getElementById('mail');
-let regexEmail = /\d\w\./g;
+let regexEmail = /[a-zA-Z0-9\-_\.]+@+[a-zA-Z0-9\-_\.]/g;
 
 valider.addEventListener('click', function valideCommande(event){
-    if(regexFirstName.test(firstName)
-        && regexLastName.test(lastName)
-        && regexAdress.test(address)
-        && regexCodeAdress.test(codeAddress)
-        && regexCity.test(city)
-        && regexEmail.test(email)
-        === true
+    if((regexFirstName.test(firstName) === true)
+        && (regexLastName.test(lastName) === true)
+        && (regexAdress.test(address) === true)
+        && (regexCodeAdress.test(codeAddress) === true)
+        && (regexCity.test(city) === true)
+        && (regexEmail.test(email) === true)
     ){
         let formulaire = {
             firstName: firstName.value,
@@ -139,12 +138,12 @@ valider.addEventListener('click', function valideCommande(event){
 
         const envoi = {
             method: 'POST',
-            body: JSON.stringify({user,commande}),
+            body: JSON.stringify({user, commande}),
             headers: {
                 'Content-Type': 'application/json'
             }
         };           
-            fetch("http://localhost:3000/api/order/"+ envoi)
+            fetch("http://localhost:3000/api/teddies/order"+ envoi)
             .then(response => response.json())
             .then(user => { 
                 console.log(user);
@@ -152,24 +151,12 @@ valider.addEventListener('click', function valideCommande(event){
             .catch(error => alert("Erreur : " + error));
         
     } else{
+        alert('Une ou plusieurs données du formulaire sont incorrectes, veuillez les vérifier !')
         window.location.href = "panier.js";
         console.log("Le formulaire n'est pas valide !")
-        alert('Une ou plusieurs données du formulaire sont incorrectes, veuillez les vérifier !')
+        
    }
 });
-
-
-
-        /**/
-    
-    
-        
-
-  
-    
-
-
-
 
 /*
 
